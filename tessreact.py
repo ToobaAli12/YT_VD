de83ae6b323580b1e9d6b9601e4fc818724a9dfe
ToobaAli12 <alizeykhan0123@gmail.com>
2023-04-14 13:02:08 +0500

    initial commit

--- tessreact.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pytesseract
-import cv2
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('2.png')
-plt.imshow(img)
-img2char = pytesseract.image_to_string(img)
-print(img2char)
